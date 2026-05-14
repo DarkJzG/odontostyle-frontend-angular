465c91f99e2d140b26427e1939863cbd9f49d222
ander2722 <espanaanderson27@gmail.com>
2026-05-13 22:58:20 -0500

    Sincronizar archivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { PerfilPaciente } from './features/Doctor/ModuloPacientes/perfilPaciente/perfilPaciente';
+import { PerfilPaciente } from './features/Doctor/ModuloPaciente/perfilPaciente/perfilPaciente';
 import { Login } from './features/Auth/login/login';
 import { Inicio } from './features/Home/inicioPagina/pages/inicioPag';
 import { PagHome } from './features/Doctor/pagHome/pagHome';
-import { ListaPaciente } from './features/Doctor/ModuloPacientes/listarPaciente/listarPaciente';
-import { RegistroPaciente } from './features/Doctor/ModuloPacientes/registroPaciente/registroPaciente';
+import { ListaPaciente } from './features/Doctor/ModuloPaciente/listarPaciente/listarPaciente';
+import { RegistroPaciente } from './features/Doctor/ModuloPaciente/registroPaciente/registroPaciente';
 
 export const routes: Routes = [
   { path: '', component: Inicio },
---
+++ src/app/features/Paciente/pagHomePaciente/pagHomePaciente.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pag-home-paciente',
+  imports: [CommonModule, Router],
+  templateUrl: './pagHomePaciente.html',
+  styleUrl: './pagHomePaciente.css',
+})
+export class PagHomePaciente {
+
+}
